245f2797130b1c387b0bc95883c4e60d904f6477
reinertraitor <raajsr35@gmail.com>
2024-03-21 22:18:22 +0530

    Q5

--- question5.py
+++ question5.py
@@ -3,3 +3,25 @@ Find out pairs with given sum value of an array
 array=[5,7,4,3,9,8,19,21]
 sum=17                                          
 '''
+
+def twosum(arr,sum1):
+    arr.sort()
+    left=0
+    right=len(arr)-1
+    
+    while(left<=right):
+        if (arr[left]+arr[right]>sum1):
+            right=right-1
+        elif (arr[left]+arr[right]<sum1):
+            left=left+1
+        elif (arr[left]+arr[right]==sum1):
+            print("Values are: ",arr[left],arr[right])
+            left=left+1
+            right=right-1
+
+arr=[5,7,4,3,9,8,19,21]
+sum1=17
+
+twosum(arr,sum1)
+    
+
